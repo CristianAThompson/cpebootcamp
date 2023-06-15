2656d98a4ed42335c61a97809be0fb6603896e99
Cristian Thompson <Thompsonacristian@gmail.com>
2023-06-15 19:15:09 +0000

    Update calendarData.js

--- docs/assets/js/calendarData.js
+++ docs/assets/js/calendarData.js
@@ -136,11 +136,11 @@ const weeks2023 = {
         comprehensive: true
     },
     33: {
-        booking_count: 2,
+        booking_count: 0,
         comprehensive: false
     },
     34: {
-        booking_count: 2,
+        booking_count: 0,
         comprehensive: true
     },
     35: {
